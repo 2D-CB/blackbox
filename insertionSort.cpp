038d74e0e037ad1d54f8847d16896818b7277fdb
Dedipyaman Das <2D_codeblocks@bitbucket.org>
2016-10-08 18:31:37 +0530

    Added insertion sorts (reverse and usual) from clrs scripts

---
+++ insertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A[5]={32,21,43,55,33};
+  int key, j;
+  //set up the loop
+  for (int i=1; i<=4; i++) {
+    key=A[i];
+    j=i-1;
+
+    while (A[j]>key) {
+      A[j+1]=A[j];
+      j=j-1;
+    }
+    A[j+1]=key;
+  }
+  
+  for (int k=0; k<=4; k++) {
+    cout <<  A[k] << " ";
+  }
+  return 0;
+  }
+  
